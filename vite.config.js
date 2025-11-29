032749fa35a2e1631afbbce7a3558129ead338a9
Clara Furini <48487549+ClaraFurini@users.noreply.github.com>
2025-11-29 17:55:53 -0300

    Configura base do Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Garantir que os assets sejam resolvidos no caminho correto ao servir pelo GitHub Pages
+  base: '/Projeto1FullStack/',
   plugins: [react()],
 })
